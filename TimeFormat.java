b552b2d5f424c057729341ae2895a9c8f3083efd
Noam Kraus-Feder <noam.krausfeder@post.runi.ac.il>
2024-11-06 18:38:36 +0200

    Completed Assignment TimeFormat

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,22 +3,25 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+		
+		//Creating a string that holds the value of minutes, so that
+		//if the value is less than 10, the first index is 0.
+		String min = "";
+		if (minutes<10) {
+			min += "0" + minutes;
+		} else {
+			min += minutes;
+		}
+
 		//If the time ranges of AM and PM are 12 hours, and i want to stay in the AM hours, i
 		//need to deduct the PM hours by 12
-		
-		if (hours >= 12) {
+		if (hours == 12) {
+			System.out.println(hours + ":" + min + " PM");
+		} else if (hours > 12) {
 			hours-=12;
-			if (minutes<10){
-				System.out.println(hours + ":0" + minutes + " PM");
-			} else{
-				System.out.println(hours + ":" + minutes + " PM");
-			}
+			System.out.println(hours + ":" + min + " PM");
 		} else {
-			if (minutes<10){
-				System.out.println(hours + ":0" + minutes + " AM");
-			} else{
-				System.out.println(hours + ":" + minutes + " AM");
-			}			
+			System.out.println(hours + ":" + min + " AM");		
 		}
 	}
 }
