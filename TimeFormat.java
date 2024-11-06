371e63b6e65898fe319dac744899edce07b4e9c9
Noam Kraus-Feder <noam.krausfeder@post.runi.ac.il>
2024-11-06 16:06:22 +0200

    Completed exercise TimeFormat

--- TimeFormat.java
+++ TimeFormat.java
@@ -2,10 +2,10 @@
 public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
 		System.out.println(hours + ":" + minutes);
+		//If the time ranges of AM and PM are 12 hours, and i want to stay in the AM hours, i
+		//need to deduct the PM hours by 12
 		if (hours >= 12) hours-=12;
 
 		System.out.println(hours + ":" + minutes);
